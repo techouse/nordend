55e59b1b37fa2a025867912766f55aba1409a60a
Klemen Tušar <techouse@gmail.com>
2018-11-24 18:33:49 +0100

    Templates

--- kilc/routes.py
+++ kilc/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from kilc import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Oh Hai!'
+    context = {'user': {'name': 'Klemen'}}
+
+    return render_template('index.html', **context)
