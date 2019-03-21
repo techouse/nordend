3f6726df4a14b71e86ef9ebf49f4c6331fa7c40f
Klemen Tušar <techouse@gmail.com>
2019-03-21 11:26:12 +0100

    Admin SPA Add automatic token refresh 1 minute before expiry

--- app/static/js/backend/store/modules/auth.js
+++ app/static/js/backend/store/modules/auth.js
@@ -1,10 +1,12 @@
-import api    from "../../components/api"
-import router from "../../router"
+import api                                           from "../../components/api"
+import router                                        from "../../router"
+import {parse, subMinutes, differenceInMilliseconds} from "date-fns"
 
 const state = {
-    userId:     null,
-    token:      null,
-    expiration: 0
+    userId:        null,
+    token:         null,
+    expiration:    0,
+    authRefresher: null
 }
 
 const getters = {
@@ -22,20 +24,29 @@ const getters = {
 }
 
 const mutations = {
-    setAuthData(state, {userId, token, expiration}) {
+    setAuthData(state, {userId, token, expiration, refresher}) {
         state.userId = userId
         state.token = token
         state.expiration = expiration
+        state.authRefresher = refresher
 
         localStorage.setItem("userId", userId)
         localStorage.setItem("token", token)
         localStorage.setItem("expiration", expiration)
     },
 
+    setAuthRefresher(state, refresher) {
+        state.authRefresher = refresher
+    },
+
     clearAuthData(state) {
         state.userId = null
         state.token = null
         state.expiration = 0
+        if (state.authRefresher !== null) {
+            window.clearTimeout(state.authRefresher)
+            state.authRefresher = null
+        }
 
         localStorage.removeItem("userId")
         localStorage.removeItem("token")
@@ -44,7 +55,7 @@ const mutations = {
 }
 
 const actions = {
-    login({state, commit, dispatch}, {email, password}) {
+    login({commit, dispatch}, {email, password}) {
         dispatch("alert/clear", null, {root: true})
 
         return new Promise((resolve, reject) => {
@@ -60,17 +71,16 @@ const actions = {
                        const jwtData = JSON.parse(atob(data.token.split(".")[0]))
                        const jwtUser = JSON.parse(atob(data.token.split(".")[1]))
 
-                       commit("setAuthData", {
+                       const authData = {
                            userId:     (jwtUser && "id" in jwtUser) ? Number(jwtUser.id) : null,
                            token:      data.token,
                            expiration: jwtData ? Number(jwtData.exp) * 1000 : 0
-                       })
+                       }
 
-                       resolve({
-                                   userId:     state.userId,
-                                   token:      state.token,
-                                   expiration: state.expiration
-                               })
+                       commit("setAuthData", authData)
+                       dispatch("refreshToken")
+
+                       resolve(authData)
                    }
                })
                .catch(error => {
@@ -84,7 +94,7 @@ const actions = {
         })
     },
 
-    autoLogin({commit, state}) {
+    autoLogin({commit, dispatch}) {
         return new Promise((resolve, reject) => {
             const userId = Number(localStorage.getItem("userId"))
             const token = localStorage.getItem("token")
@@ -95,23 +105,58 @@ const actions = {
                 return
             }
 
-            commit("setAuthData", {
+            const authData = {
                 userId:     userId,
                 token:      token,
                 expiration: expiration
-            })
+            }
 
-            resolve({
-                        userId:     state.userId,
-                        token:      state.token,
-                        expiration: state.expiration
-                    })
+            commit("setAuthData", authData)
+            dispatch("refreshToken")
+
+            resolve(authData)
         })
     },
 
     logout({commit}) {
         commit("clearAuthData")
         router.replace({name: "Login"})
+    },
+
+    refreshToken({state, commit, dispatch}) {
+        const refresher = setTimeout(() => {
+                                         api.post("/login/", {}, {
+                                                auth: {
+                                                    username: state.token,
+                                                    password: ""
+                                                }
+                                            })
+                                            .then(response => {
+                                                const data = response.data
+                                                if (data.token) {
+                                                    const jwtData = JSON.parse(atob(data.token.split(".")[0]))
+                                                    const jwtUser = JSON.parse(atob(data.token.split(".")[1]))
+
+                                                    const authData = {
+                                                        userId:     (jwtUser && "id" in jwtUser) ? Number(jwtUser.id) : null,
+                                                        token:      data.token,
+                                                        expiration: jwtData ? Number(jwtData.exp) * 1000 : 0
+                                                    }
+
+                                                    commit("setAuthData", authData)
+                                                }
+                                            })
+                                            .catch(error => {
+                                                try {
+                                                    dispatch("alert/error", error.response.data.message, {root: true})
+                                                } catch (e) {
+                                                    console.log(error)
+                                                }
+                                            })
+                                     },
+                                     differenceInMilliseconds(subMinutes(parse(state.expiration), 1), new Date()))
+
+        commit("setAuthRefresher", refresher)
     }
 }
 
