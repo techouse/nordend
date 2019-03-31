ba4a82bfd9decb758e5efd5469e3f40343193f12
Klemen Tušar <techouse@gmail.com>
2019-03-31 15:59:22 +0200

    Admin SPA Remove leftovers

--- app/static/js/backend/models/Role.js
+++ app/static/js/backend/models/Role.js
@@ -24,7 +24,6 @@ export default class Role {
         return {
             name:        this.name,
             default:     this.default,
-            follow:      this.follow,
             permissions: permissions
         }
     }
