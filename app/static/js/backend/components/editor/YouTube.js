c1567be10fc8e3dee7bacd270fcbdf722df6130d
Klemen Tušar <techouse@gmail.com>
2019-05-06 13:05:16 +0200

    WIP YouTube popover

--- app/static/js/backend/components/editor/YouTube.js
+++ app/static/js/backend/components/editor/YouTube.js
@@ -1,6 +1,14 @@
 import {Node}       from "tiptap"
 import {mapActions} from "vuex"
 import axios        from "axios"
+import {
+    getHours,
+    getMinutes,
+    getSeconds,
+    setHours,
+    setMinutes,
+    setSeconds
+}                   from "date-fns"
 
 export function getYouTubeId(url) {
     const re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i
@@ -95,7 +103,21 @@ export default class YouTube extends Node {
             props:    ["node", "updateAttrs", "editable"],
             data() {
                 return {
-                    youTubeId: null
+                    loading:        false,
+                    popoverVisible: false,
+                    youTubeId:      null,
+                    aspectRatio:    16 / 9,
+                    rules:          {
+                        url:    [
+                            {validator: this.validateYouTubeUrl, trigger: "blur"},
+                        ],
+                        width:  [
+                            {validator: this.validateWidth, trigger: "blur"}
+                        ],
+                        height: [
+                            {validator: this.validateHeight, trigger: "blur"}
+                        ]
+                    }
                 }
             },
             created() {
@@ -105,8 +127,17 @@ export default class YouTube extends Node {
                 src() {
                     return this.node.attrs.src
                 },
-                start() {
-                    return this.node.attrs.start
+                start: {
+                    get() {
+                        return setSeconds(setMinutes(setHours(new Date(), 0), 0), Number(this.node.attrs.start))
+                    },
+                    set(start) {
+                        this.updateAttrs(
+                            {
+                                start: getHours(start) * 3600 + getMinutes(start) * 60 + getSeconds(start)
+                            }
+                        )
+                    }
                 },
                 width() {
                     return this.node.attrs.width
@@ -114,33 +145,131 @@ export default class YouTube extends Node {
                 height() {
                     return this.node.attrs.height
                 },
-                url: {
+                url:   {
                     get() {
-                        return this.node.attrs.start ? `https://www.youtube.com/watch?v=${this.youTubeId}&t=${this.node.attrs.start}`
-                                                     : `https://www.youtube.com/watch?v=${this.youTubeId}`
+                        return this.node.attrs.src
                     },
                     set(src) {
                         this.$set(this, "youTubeId", getYouTubeId(src))
-                        if (this.youTubeId) {
-                            this.updateAttrs(
-                                {
-                                    src: this.node.attrs.start ? `https://www.youtube.com/embed/${this.youTubeId}?start=${this.node.attrs.start}`
-                                                               : `https://www.youtube.com/embed/${this.youTubeId}`
-                                }
-                            )
-                        }
+                        this.updateAttrs(
+                            {
+                                src: this.youTubeId ? `https://www.youtube.com/embed/${this.youTubeId}` : src
+                            }
+                        )
                     },
                 },
                 imageSrc() {
                     return `https://img.youtube.com/vi/${this.youTubeId}/0.jpg`
+                },
+                formRef() {
+                    return `youtube_${this.node.attrs["src"]}`
+                },
+                form() {
+                    return {
+                        url:    this.url,
+                        start:  this.start,
+                        width:  this.width,
+                        height: this.height
+                    }
                 }
             },
             methods:  {
                 ...mapActions("alert", ["error"]),
+                showPopover() {
+                    this.$set(this, "popoverVisible", true)
+                },
+                hidePopover() {
+                    this.$set(this, "popoverVisible", false)
+                    this.$set(this, "loading", false)
+                },
+                changeWidth(width) {
+                    this.updateAttrs({width})
+                    this.updateAttrs({height: Math.round(width / this.aspectRatio)})
+                },
+
+                changeHeight(height) {
+                    this.updateAttrs({height})
+                    this.updateAttrs({width: Math.round(height * this.aspectRatio)})
+                },
+
+                changeStart(start) {
+                    if (start === null) {
+                        this.$set(this, "start", setSeconds(setMinutes(setHours(new Date(), 0), 0), 0))
+                        this.$set(this.form, "start", null)
+                    }
+                },
+
+                validateYouTubeUrl(rule, value, callback) {
+                    if (!value) {
+                        callback(new Error("Please input a YouTube URL"))
+                    } else if (!getYouTubeId(value)) {
+                        callback(new Error("The YouTube URL is invalid."))
+                    } else {
+                        callback()
+                    }
+                },
+
+                validateWidth(rule, value, callback) {
+                    if (Number(value) <= 0) {
+                        callback(new Error("Please input a YouTube video width"))
+                    } else if (Number(value) < 426 || Number(value) > 3840) {
+                        callback(new Error("Please input a YouTube video width between 426 and 3840"))
+                    } else {
+                        callback()
+                    }
+                },
+
+                validateHeight(rule, value, callback) {
+                    if (Number(value) <= 0) {
+                        callback(new Error("Please input a YouTube video height"))
+                    } else if (Number(value) < 240 || Number(value) > 2160) {
+                        callback(new Error("Please input a YouTube video height between 240 and 2160"))
+                    } else {
+                        callback()
+                    }
+                },
             },
             template: `
-                <el-card :body-style="{ padding: 0 }" :style="{width: width + 'px'}">
-                    <el-image :src="imageSrc" 
+                <el-popover placement="top"
+                            width="600"
+                            trigger="click"
+                            title="YouTube video details"
+                            :disabled="!editable"
+                            @show="showPopover"
+                            @hide="hidePopover">
+                    <el-form v-if="editable" :model="form" :ref="formRef" :rules="rules" label-placement="right" label-width="120px">
+                        <el-form-item label="YouTube URL" prop="url">
+                            <el-input type="url" v-model="url" style="width: calc(100% - 120px)"/>
+                        </el-form-item>
+                        <el-form-item label="Start at" prop="start">
+                            <el-time-picker v-model="start"
+                                            :picker-options="{format: 'HH:mm:ss'}"
+                                            placeholder="Start at"
+                                            :disabled="!youTubeId"
+                                            @change="changeStart">
+                            </el-time-picker>
+                        </el-form-item>
+                        <el-form-item prop="width" label="Width">
+                            <el-input-number :value="width"
+                                             :min="426"
+                                             :max="3840"
+                                             :disabled="!youTubeId"
+                                             required
+                                             @change="changeWidth"
+                            />
+                        </el-form-item>
+                        <el-form-item prop="height" label="Height">
+                            <el-input-number :value="height"
+                                             :min="240"
+                                             :max="2160"
+                                             :disabled="!youTubeId"
+                                             required
+                                             @change="changeHeight"
+                            />
+                        </el-form-item>
+                    </el-form>
+                    <el-image slot="reference"
+                              :src="imageSrc" 
                               :style="{width: width + 'px', height: height + 'px'}" 
                               :alt="url" 
                               :title="url" 
@@ -152,10 +281,7 @@ export default class YouTube extends Node {
                             <i class="el-icon-picture-outline"></i>
                         </div>
                     </el-image>
-                    <div style="padding: 14px;">
-                        <input class="iframe__input" @paste.stop type="url" v-model="url" v-if="editable" required />
-                    </div>
-                </el-card>
+                </el-popover>
             `,
         }
     }
