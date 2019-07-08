030335d8fdd36ccb6cef6d5226ef1401c3f424fe
Klemen Tušar <techouse@gmail.com>
2019-07-09 00:04:35 +0200

    :bug: Fix token refresh to work with reCAPTCHA

--- app/decorators.py
+++ app/decorators.py
@@ -33,20 +33,23 @@ def verify_recaptcha(f):
                 and current_app.config["RECAPTCHA_SITE_KEY"]
                 and current_app.config["RECAPTCHA_SECRET_KEY"]
         ):
-            data = request.get_json() if request.get_json() else request.data
-            recaptcha_token = data.get("recaptcha_token")
-            if recaptcha_token:
-                r = requests.post(
-                    "https://www.google.com/recaptcha/api/siteverify",
-                    data={
-                        "secret": current_app.config["RECAPTCHA_SECRET_KEY"],
-                        "response": recaptcha_token,
-                        "remoteip": request.access_route[0],
-                    },
-                )
-                result = r.json()
-                g.recaptcha_valid = result.get("success") or False
-            else:
+            request_data = request.get_json() if request.get_json() else request.data
+            try:
+                recaptcha_token = request_data.get("recaptcha_token")
+                if recaptcha_token:
+                    r = requests.post(
+                        "https://www.google.com/recaptcha/api/siteverify",
+                        data={
+                            "secret": current_app.config["RECAPTCHA_SECRET_KEY"],
+                            "response": recaptcha_token,
+                            "remoteip": request.access_route[0],
+                        },
+                    )
+                    result = r.json()
+                    g.recaptcha_valid = result.get("success") or False
+                else:
+                    g.recaptcha_valid = False
+            except AttributeError:
                 g.recaptcha_valid = False
 
         return f(*args, **kwargs)
--- app/static/js/backend/store/modules/auth.js
+++ app/static/js/backend/store/modules/auth.js
@@ -190,31 +190,64 @@ const actions = {
     refreshToken({state, commit, dispatch}) {
         const refresher = setTimeout(() => {
                                          dispatch("csrf/getCsrf", {}, {root: true}).then(() => {
-                                             api.post("login", {}, {
-                                                    auth: {
-                                                        username: state.token,
-                                                        password: ""
-                                                    }
-                                                })
-                                                .then(response => {
-                                                    const data = response.data
-                                                    if (data.token) {
-                                                        const jwtData = JSON.parse(atob(data.token.split(".")[0]))
-                                                        const jwtUser = JSON.parse(atob(data.token.split(".")[1]))
+                                             if (window.reCAPTCHASiteKey) {
+                                                 grecaptcha.ready(() => {
+                                                     grecaptcha.execute(window.reCAPTCHASiteKey, {action: "refresh_token"})
+                                                               .then(token => {
+                                                                   api.post("login", {recaptcha_token: token}, {
+                                                                          auth: {
+                                                                              username: state.token,
+                                                                              password: ""
+                                                                          }
+                                                                      })
+                                                                      .then(response => {
+                                                                          const data = response.data
+                                                                          if (data.token) {
+                                                                              const jwtData = JSON.parse(atob(data.token.split(".")[0]))
+                                                                              const jwtUser = JSON.parse(atob(data.token.split(".")[1]))
 
-                                                        const authData = {
-                                                            userId:     (jwtUser && "id" in jwtUser) ? Number(jwtUser.id) : null,
-                                                            token:      data.token,
-                                                            expiration: jwtData ? Number(jwtData.exp) * 1000 : 0
+                                                                              const authData = {
+                                                                                  userId:     (jwtUser && "id" in jwtUser) ? Number(jwtUser.id) : null,
+                                                                                  token:      data.token,
+                                                                                  expiration: jwtData ? Number(jwtData.exp) * 1000 : 0
+                                                                              }
+
+                                                                              commit("setAuthData", authData)
+                                                                              dispatch("refreshToken")
+                                                                          }
+                                                                      })
+                                                                      .catch(() => {
+                                                                          dispatch("logout")
+                                                                      })
+                                                               })
+                                                 })
+                                             } else {
+                                                 api.post("login", {}, {
+                                                        auth: {
+                                                            username: state.token,
+                                                            password: ""
                                                         }
+                                                    })
+                                                    .then(response => {
+                                                        const data = response.data
+                                                        if (data.token) {
+                                                            const jwtData = JSON.parse(atob(data.token.split(".")[0]))
+                                                            const jwtUser = JSON.parse(atob(data.token.split(".")[1]))
 
-                                                        commit("setAuthData", authData)
-                                                        dispatch("refreshToken")
-                                                    }
-                                                })
-                                                .catch(() => {
-                                                    dispatch("logout")
-                                                })
+                                                            const authData = {
+                                                                userId:     (jwtUser && "id" in jwtUser) ? Number(jwtUser.id) : null,
+                                                                token:      data.token,
+                                                                expiration: jwtData ? Number(jwtData.exp) * 1000 : 0
+                                                            }
+
+                                                            commit("setAuthData", authData)
+                                                            dispatch("refreshToken")
+                                                        }
+                                                    })
+                                                    .catch(() => {
+                                                        dispatch("logout")
+                                                    })
+                                             }
                                          })
                                      },
                                      Math.max(differenceInMilliseconds(subMinutes(parse(state.expiration), state.authRefreshThreshold), new Date()), 1000))
